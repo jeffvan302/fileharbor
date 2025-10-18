4448100893820bfd626de480f14ba074a17eb618
Theunis van Niekerk <jeff@nardan.net>
2025-10-18 02:38:55 -0400

    tests

--- my_test.py
+++ my_test.py
@@ -1,9 +1,9 @@
 from fileharbor import FileHarborClient
 
 # Connect and use
-with FileHarborClient("cli_config.json") as client:
+with FileHarborClient("client_config.json") as client:
     # Upload file
-    client.upload("local.txt", "remote.txt", show_progress=True)
+    client.upload("pic_test.png", "remote_pic.png", show_progress=True)
 
     # Download file
-    client.download("remote.txt", "copy.txt", show_progress=True)
+    client.download("remote_pic.png", "copy_pic.png", show_progress=True)
